a5de5a08a4cbbc11a2d8d74b3bf18ffe98268e3e
Sargon Piraev <sargnpiraev@gmail.com>
2019-05-22 16:59:46 +0300

    RD-5

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -1,4 +1,3 @@
-const laborxScAbiConfig = require('@laborx/sc-abi')
 const EventEmitter = require('safe-event-emitter')
 const ObservableStore = require('obs-store')
 const ethUtil = require('ethereumjs-util')
@@ -7,7 +6,6 @@ const EthQuery = require('ethjs-query')
 const abi = require('human-standard-token-abi')
 const abiDecoder = require('abi-decoder')
 abiDecoder.addABI(abi)
-// abiDecoder.addABI(laborxScAbiConfig.JobController.abi)
 const {
   TOKEN_METHOD_APPROVE,
   TOKEN_METHOD_TRANSFER,
